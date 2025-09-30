ba208e552dfaa9057434432a4477531c8315d966
abi <abi.oluwade@gmail.com>
2025-10-01 00:30:38 +0100

    moved replace method logic within update / now working.

--- app/src/main/java/org/sandbox/App.java
+++ app/src/main/java/org/sandbox/App.java
@@ -1,16 +1,11 @@
 package org.sandbox;
 
-import java.io.*;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-//        PushParameters.readFileAndPushParameters(args[0]);
-//        Replace.updateConfigFile(args[0], args[1]);
-        Replace.replacePlaceholders(args[0]);
-
-
+        PushParameters.readFileAndPushParameters(args[0]);
+        Replace.updateConfigFile(args[0], args[1]);
 
     }
 
--- app/src/main/java/org/sandbox/Replace.java
+++ app/src/main/java/org/sandbox/Replace.java
@@ -10,6 +10,11 @@ public class Replace {
 
     public static void updateConfigFile(String readFile, String writeFile) throws IOException {
 
+        SsmClient ssmClient = SsmClient
+                .builder()
+                .credentialsProvider(Connection.createConnection())
+                .build();
+
         BufferedReader file_read = new BufferedReader(new FileReader(readFile));
         String line;
 
@@ -22,12 +27,25 @@ public class Replace {
             int index_start = line.indexOf("=");
             int index_end = line.lastIndexOf("@");
 
+            int index_end_placeholder = line.indexOf("=");
+
+            GetParametersRequest getParametersRequest = GetParametersRequest
+                    .builder()
+                    .names("/NONPROD/" + line.substring(0, index_end_placeholder))
+                    .withDecryption(true)
+                    .build();
+
+            GetParametersResponse getParametersResponse = ssmClient.getParameters(getParametersRequest);
+
+            String value = getParametersResponse.parameters().get(0).value();
+
             file_builder.insert(0,line);
-            file_builder.replace(index_start + 1,index_end + 1,"VALUE\n");
+            file_builder.replace(index_start + 1,index_end + 1,value + "\n");
 
         }
 
         updated_file = file_builder.toString();
+
         System.out.println(updated_file);
 
         file_write.write(updated_file);
@@ -37,35 +55,5 @@ public class Replace {
 
     }
 
-    public static void replacePlaceholders(String readFile) throws IOException {
-
-        SsmClient ssmClient = SsmClient
-                .builder()
-                .credentialsProvider(Connection.createConnection())
-                .build();
-
-        BufferedReader file_read = new BufferedReader(new FileReader(readFile));
-        String line;
-
-        while((line = file_read.readLine()) != null) {
-
-            int index_end = line.indexOf("=");
-
-            GetParametersRequest getParametersRequest = GetParametersRequest
-                    .builder()
-                    .names("/NONPROD/" + line.substring(0, index_end))
-                    .withDecryption(true)
-                    .build();
-
-            GetParametersResponse getParametersResponse = ssmClient.getParameters(getParametersRequest);
-
-            String name = (getParametersResponse.parameters().get(0).name().substring(9));
-            String value = getParametersResponse.parameters().get(0).value();
-
-        }
-
-
-
-    }
 
 }
